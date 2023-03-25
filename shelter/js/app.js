c020125f986b118b280ca1e25298ece191d93374
FreightDH <maxim.zolotkov@yandex.ru>
2023-03-26 01:57:07 +0300

    feat: done the first page

---
+++ shelter/js/app.js
@@ -0,0 +1,17 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    isWebp();
+})();
