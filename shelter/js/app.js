d9d80748d95ee324aa13cc0ded8656597cbc1cf1
FreightDH <maxim.zolotkov@yandex.ru>
2023-04-10 01:02:26 +0300

    feat: add slider (without popup & reload)

--- shelter/js/app.js
+++ shelter/js/app.js
@@ -73,6 +73,76 @@
             }
         }));
     }
+    const arrowLeft = document.querySelector(".arrow--l");
+    const arrowRight = document.querySelector(".arrow--r");
+    const slider = document.querySelector(".slider__wrapper");
+    let columnLeft = document.querySelector(".column-left");
+    let columnRight = document.querySelector(".column-right");
+    let petsInfo;
+    let activeNames = [];
+    let prevActiveNames = [];
+    const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+    (async () => {
+        const file = "files/pets.json";
+        const result = await fetch(file);
+        petsInfo = await result.json();
+    })();
+    function createCard(i) {
+        let index;
+        do {
+            index = getRandomNum(1, 8);
+        } while (activeNames.includes(`${petsInfo[index]["name"]}`) || prevActiveNames.includes(`${petsInfo[index]["name"]}`));
+        const card = document.createElement("div");
+        card.classList.add("pets__column");
+        card.classList.add(`column--${++i}`);
+        card.innerHTML = `\n\t\t\t\t\t\t<div class="pets__card card popup-link">\n\t\t\t\t\t\t\t<div class="card__image"><img src="${petsInfo[index]["img"]}" alt="pet"></div>\n\t\t\t\t\t\t\t<div class="card__name">${petsInfo[index]["name"]}</div>\n\t\t\t\t\t\t\t<a href="#" class="card__button btn btn--w"><span>Learn more</span></a>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t`;
+        activeNames.push(petsInfo[index]["name"]);
+        return card;
+    }
+    const moveLeft = () => {
+        prevActiveNames = [];
+        for (let petsColumn of columnLeft.children) {
+            let card = petsColumn.children[0];
+            prevActiveNames.push(card.children[1].textContent);
+        }
+        slider.classList.add("move-left");
+        arrowLeft.removeEventListener("click", moveLeft);
+        arrowRight.removeEventListener("click", moveRight);
+    };
+    const moveRight = () => {
+        prevActiveNames = [];
+        for (let petsColumn of columnRight.children) {
+            let card = petsColumn.children[0];
+            prevActiveNames.push(card.children[1].textContent);
+        }
+        slider.classList.add("move-right");
+        arrowLeft.removeEventListener("click", moveLeft);
+        arrowRight.removeEventListener("click", moveRight);
+    };
+    arrowLeft.addEventListener("click", moveLeft);
+    arrowRight.addEventListener("click", moveRight);
+    slider.addEventListener("animationend", (animationEvent => {
+        if ("move-left" === animationEvent.animationName) {
+            slider.classList.remove("move-left");
+            document.querySelector(".column-active").innerHTML = columnLeft.innerHTML;
+        } else {
+            slider.classList.remove("move-right");
+            document.querySelector(".column-active").innerHTML = columnRight.innerHTML;
+        }
+        columnLeft = changeColumn(columnLeft);
+        columnRight = changeColumn(columnRight);
+        arrowLeft.addEventListener("click", moveLeft);
+        arrowRight.addEventListener("click", moveRight);
+    }));
+    function changeColumn(column) {
+        column.innerHTML = "";
+        activeNames = [];
+        for (let i = 0; i < 3; i++) {
+            const card = createCard(i);
+            column.append(card);
+        }
+        return column;
+    }
     const popupLinks = document.querySelectorAll(".popup-link");
     const closeIcon = document.querySelector(".popup__close");
     const popupImage = document.querySelector(".popup__image");
@@ -123,9 +193,6 @@
         popupList[2].lastChild.textContent = petsInfo[infoIndex]["diseases"].join(", ");
         popupList[3].lastChild.textContent = petsInfo[infoIndex]["parasites"].join(", ");
     }
-    document.querySelector(".arrow--l");
-    document.querySelector(".arrow--r");
-    document.querySelectorAll(".pets__card");
     isWebp();
     menuInit();
 })();
