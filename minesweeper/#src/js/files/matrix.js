4201935ab951774f20888f053233583392fd590d
FreightDH <maxim.zolotkov@yandex.ru>
2023-05-17 18:13:08 +0300

    feat: add count of bombs function

--- minesweeper/#src/js/files/matrix.js
+++ minesweeper/#src/js/files/matrix.js
@@ -1,11 +1,13 @@
 import { getRandomNumber } from "./functions.js";
 
 const cells = document.querySelectorAll('.field__cell');
+const bombTag = '<img src="./img/bomb.png" alt="bomb">';
+const flagTag = '<img src="./img/flag.png" alt="flag">';
 
 export let matrix = [];
 
 function isBomb(cell) {
-  return cell === 1 ? true : false;
+  return cell === -1 ? true : false;
 }
 
 function setBombs(bombCount) {
@@ -14,7 +16,7 @@ function setBombs(bombCount) {
     const y = getRandomNumber(0, 9);
     
     if (!isBomb(matrix[x][y])) {
-      matrix[x][y] = 1;
+      matrix[x][y] = -1;
       bombCount--;
     }
   }
@@ -22,7 +24,45 @@ function setBombs(bombCount) {
 
 function fillField() {
   const matrixCopy = ([].concat(...matrix));
-  cells.forEach((cell, index) => cell.textContent = matrixCopy[index]);
+
+  cells.forEach((cell, index) => {
+    const cellValue = matrixCopy[index];
+    
+    if (isBomb(cellValue)) {
+      cell = cell.insertAdjacentHTML('afterbegin', bombTag);
+    } else {
+      if (cellValue > 0) {
+        cell.textContent = cellValue;
+        cell.classList.add(`color--${cellValue}`);
+      } 
+    }
+  });
+}
+
+function countBombs() {
+  const N = matrix.length;
+	const M = matrix[0].length;
+	let count = 0;
+	
+	for (let i = 0; i < N; i++) {
+    for(let j = 0; j < M; j++) {
+      count = 0;
+      if (!isBomb(matrix[i][j])) {
+        // МИНЫ РЯДОМ
+        if(i > 0 && matrix[i - 1][j] === -1) count++;
+        if(i < N - 1 && matrix[i + 1][j] === -1) count++;
+        if(j > 0 && matrix[i][j - 1] === -1) count++;
+        if(j < M - 1 && matrix[i][j + 1] === -1) count++;
+        // МИНЫ ПО УГЛАМ
+        if(i > 0 && j > 0 && matrix[i - 1][j - 1] === -1) count++;
+        if(i > 0 && j <= M - 1 && matrix[i - 1][j + 1] === -1) count++;
+        if(i < N - 1 && j > 0 && matrix[i + 1][j - 1] === -1) count++;
+        if(i < N - 1 && j < M - 1 && matrix[i + 1][j + 1] === -1) count++;
+
+        matrix[i][j] = count;
+      }
+    }
+  }
 }
 
 function generateMatrix(width = 10, height = 10, bombCount = 10) {
@@ -31,9 +71,16 @@ function generateMatrix(width = 10, height = 10, bombCount = 10) {
   );
 
   setBombs(bombCount);
+  countBombs();
   fillField();
 
   console.log(matrix);
 }
+
+cells.forEach(cell => {
+  cell.addEventListener('click', function (event) {
+    cell.classList.add('opened');
+  });
+})
 
 generateMatrix();
