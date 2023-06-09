f8f7b3a3a054378141b4032fcaacfe289f2369f9
FreightDH <maxim.zolotkov@yandex.ru>
2023-06-09 18:38:37 +0300

    feat: add prettier source files with api key

---
+++ news-api/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+  constructor() {
+    super('http://127.0.0.1:8075/', {
+      apiKey: 'e701106342834977a2958d1a769d7859',
+    });
+  }
+}
+
+export default AppLoader;
