84641de5c4b189f29a8243e61b73f5389dda7cf3
FreightDH <maxim.zolotkov@yandex.ru>
2023-06-11 13:41:20 +0300

    refactor: selectors in news.ts

--- news-api/src/components/view/news/news.ts
+++ news-api/src/components/view/news/news.ts
@@ -5,20 +5,20 @@ class News {
   public draw(data: Article[]): void {
     const news: Article[] = data.length >= 10 ? data.filter((_item, index) => index < 10) : data;
     const fragment: DocumentFragment = document.createDocumentFragment();
-    const newsItemTemp: HTMLTemplateElement = document.querySelector('#newsItemTemp')!;
+    const newsItemTemp: HTMLTemplateElement = document.querySelector('#news__item--template')!;
 
     news.forEach((item, index) => {
       const newsClone: DocumentFragment = newsItemTemp.content.cloneNode(true) as DocumentFragment;
 
       if (index % 2) newsClone.querySelector('.news__item')?.classList.add('alt');
 
-      const newsPhoto: HTMLElement = newsClone.querySelector('.news__meta-photo')!;
-      const newsAuthor: Element = newsClone.querySelector('.news__meta-author')!;
-      const newsDate: Element = newsClone.querySelector('.news__meta-date')!;
-      const newsTitle: Element = newsClone.querySelector('.news__description-title')!;
-      const newsSource: Element = newsClone.querySelector('.news__description-source')!;
-      const newsContent: Element = newsClone.querySelector('.news__description-content')!;
-      const newsReadMore: Element = newsClone.querySelector('.news__read-more a')!;
+      const newsPhoto: HTMLElement = newsClone.querySelector('.meta__photo')!;
+      const newsAuthor: Element = newsClone.querySelector('.meta__author')!;
+      const newsDate: Element = newsClone.querySelector('.meta__date')!;
+      const newsTitle: Element = newsClone.querySelector('.description__title')!;
+      const newsSource: Element = newsClone.querySelector('.description__source')!;
+      const newsContent: Element = newsClone.querySelector('.description__content')!;
+      const newsReadMore: Element = newsClone.querySelector('.description__readmore a')!;
 
       newsPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
       newsAuthor.textContent = item.author || item.source.name;
