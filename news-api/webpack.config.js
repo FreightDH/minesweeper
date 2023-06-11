60eabcfd324b0e791dcd1acb726e0be258ed3beb
FreightDH <maxim.zolotkov@yandex.ru>
2023-06-11 13:14:53 +0300

    feat: add scss to webpack config, rewrite header & footer in index.html

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -13,8 +13,8 @@ const baseConfig = {
         use: 'ts-loader',
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(scss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
