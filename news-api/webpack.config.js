033e5dd91c4a26d928fdcdebecdeb987cf3577f0
FreightDH <maxim.zolotkov@yandex.ru>
2023-06-13 01:15:02 +0300

    feat: build project

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -23,7 +23,7 @@ const baseConfig = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, './dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
