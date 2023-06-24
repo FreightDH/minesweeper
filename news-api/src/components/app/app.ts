c4cef4cdcfc7d514034a64c7d813593555a74f7a
Max Zolotkov <maxim.zolotkov@yandex.ru>
2023-06-24 18:02:25 +0300

    fix: remove reduntant typing from app.ts

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -3,31 +3,22 @@ import AppView from '../view/appView';
 import { Data, DataNews, DataSources } from '../types';
 
 class App {
-  public controller: AppController;
-  public view: AppView;
-
-  constructor() {
+  constructor(public controller: AppController, public view: AppView) {
     this.controller = new AppController();
     this.view = new AppView();
   }
 
-  public start(): void | never {
-    const sources: Element | null = document.querySelector('.sources__sources');
-    const sourcesFilter: Element | null = document.querySelector('.sources__filter');
+  public start(): void {
+    const sources = document.querySelector('.sources__sources')!;
+    const sourcesFilter = document.querySelector('.sources__filter')!;
 
-    if (sourcesFilter) {
-      sourcesFilter.addEventListener('click', (e: Event) => {
-        this.controller.getSources((data: Data) => this.view.drawSources(e, data as DataSources));
-      });
-    }
+    sourcesFilter.addEventListener('click', (e: Event) => {
+      this.controller.getSources((data: Data) => this.view.drawSources(e, data as DataSources));
+    });
 
-    if (sources) {
-      sources.addEventListener('click', (e: Event) =>
-        this.controller.getNews(e, (data: Data) => this.view.drawNews(data as DataNews)),
-      );
-    } else {
-      throw new Error("Class sources doesn't exists.");
-    }
+    sources.addEventListener('click', (e: Event) =>
+      this.controller.getNews(e, (data: Data) => this.view.drawNews(data as DataNews)),
+    );
   }
 }
 
