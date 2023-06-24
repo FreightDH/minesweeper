4fad95363b91f3460b7e748c15335266bc8569e3
Max Zolotkov <maxim.zolotkov@yandex.ru>
2023-06-24 17:34:07 +0300

    fix: remove redundant typing from sources.ts

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -2,15 +2,15 @@ import { Source } from '../../types';
 
 class Sources {
   public generateSourceFilter(): void {
-    const fragment: DocumentFragment = document.createDocumentFragment();
+    const fragment = document.createDocumentFragment();
     const sourcesFilter = document.querySelector('.sources__filter')!;
-    const filterButtonAll: Element = document.createElement('div');
+    const filterButtonAll = document.createElement('div');
 
     filterButtonAll.className = 'filter__button button--all';
     filterButtonAll.textContent = 'All';
 
     for (let i = 65; i < 91; i++) {
-      const filterButton: Element = document.createElement('div');
+      const filterButton = document.createElement('div');
       filterButton.classList.add('filter__button');
       filterButton.textContent = String.fromCharCode(i);
       fragment.append(filterButton);
@@ -21,12 +21,12 @@ class Sources {
   }
 
   public draw(data: Source[]): void {
-    const fragment: DocumentFragment = document.createDocumentFragment();
-    const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sources__item--template');
+    const fragment = document.createDocumentFragment();
+    const sourceItemTemp: HTMLTemplateElement = document.querySelector('#sources__item--template')!;
 
     data.forEach((item) => {
       if (sourceItemTemp) {
-        const sourceClone: DocumentFragment = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+        const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
         const sourceItemName = sourceClone.querySelector('.item__name');
 
         if (sourceItemName) sourceItemName.textContent = item.name;
