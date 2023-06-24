712b0773c63dbf1621a70164162640081fbf28dd
Max Zolotkov <maxim.zolotkov@yandex.ru>
2023-06-24 17:36:34 +0300

    fix: remove redundant typing from news.ts

--- news-api/src/components/view/news/news.ts
+++ news-api/src/components/view/news/news.ts
@@ -2,22 +2,22 @@ import { Article } from '../../types';
 
 class News {
   public draw(data: Article[]): void {
-    const news: Article[] = data.length >= 10 ? data.filter((_item, index) => index < 10) : data;
-    const fragment: DocumentFragment = document.createDocumentFragment();
+    const news = data.length >= 10 ? data.filter((_item, index) => index < 10) : data;
+    const fragment = document.createDocumentFragment();
     const newsItemTemp: HTMLTemplateElement = document.querySelector('#news__item--template')!;
 
     news.forEach((item, index) => {
-      const newsClone: DocumentFragment = newsItemTemp.content.cloneNode(true) as DocumentFragment;
+      const newsClone = newsItemTemp.content.cloneNode(true) as DocumentFragment;
 
       if (index % 2) newsClone.querySelector('.news__item')?.classList.add('alt');
 
       const newsPhoto: HTMLElement = newsClone.querySelector('.meta__photo')!;
-      const newsAuthor: Element = newsClone.querySelector('.meta__author')!;
-      const newsDate: Element = newsClone.querySelector('.meta__date')!;
-      const newsTitle: Element = newsClone.querySelector('.description__title')!;
-      const newsSource: Element = newsClone.querySelector('.description__source')!;
-      const newsContent: Element = newsClone.querySelector('.description__content')!;
-      const newsReadMore: Element = newsClone.querySelector('.description__readmore a')!;
+      const newsAuthor = newsClone.querySelector('.meta__author')!;
+      const newsDate = newsClone.querySelector('.meta__date')!;
+      const newsTitle = newsClone.querySelector('.description__title')!;
+      const newsSource = newsClone.querySelector('.description__source')!;
+      const newsContent = newsClone.querySelector('.description__content')!;
+      const newsReadMore = newsClone.querySelector('.description__readmore a')!;
 
       newsPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
       newsAuthor.textContent = item.author || item.source.name;
@@ -29,7 +29,7 @@ class News {
       fragment.append(newsClone);
     });
 
-    const newsElement: Element = document.querySelector('.news__body')!;
+    const newsElement = document.querySelector('.news__body')!;
 
     newsElement.innerHTML = '';
     newsElement.appendChild(fragment);
